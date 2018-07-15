109998fc25613f0dfa658dad591d3dc628da07fb
Steve Gordon <sgordon@hotmail.co.uk>
2018-07-15 10:26:24 +0100

    Sample code

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TimeWaitDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Starting connections");
+
+            for (int i = 0; i < 10; i++)
+            {
+                using (var client = new HttpClient())
+                {
+                    var result = await client.GetAsync("https://www.stevejgordon.co.uk");
+                    Console.WriteLine(result.StatusCode);
+                }
+            }
+
+            Console.WriteLine("Finished connections");
+        }
+    }
+}
